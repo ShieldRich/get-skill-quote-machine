fbe13f721ad2db8ec26330ab8e4a1a4dcb97e6bb
ShieldRich <richmeistergrad@yahoo.com>
2020-08-24 10:55:26 -0400

    updated readme, removed h1 heading

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is a random quote machine
+
+DEMO: "https://ShieldRich.github.io-get-skill-quote-machine"
--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 let quoteDBUrl = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
 
 function App() {
-  const [quote, setQuote] = useState("Work It does not matter how slowly you go as long as you do not stop.")
+  const [quote, setQuote] = useState("It does not matter how slowly you go as long as you do not stop.")
   const [author, setAuthor] = useState("Confucius")
   const [randomNumber, setRandomNumber] = useState(0)
   const [quotesArray, setquotesArray] = useState(null)
@@ -35,7 +35,6 @@ function App() {
     <div className="App">
       <header className="App-header" style={{backgroundColor: accentColor}}>
         <div id="quote-box" style={{color: accentColor}}>
-          <h1>Random Number: {randomNumber} </h1>
           <p id="text">
             "{quote}"
           </p>
