2b72e705d43b8b60bc97e141fb2031835484e2d8
ShieldRich <richmeistergrad@yahoo.com>
2020-08-24 10:47:35 -0400

    Updates

---
+++ precache-manifest.3fc0fdcbf494bf4151708644bbeb23e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3c311449cff31eb45e8bc46cb5e6e69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57f66f140ddeef0aa6ed",
+    "url": "/static/css/main.87481de0.chunk.css"
+  },
+  {
+    "revision": "3157829e6f1b5879beab",
+    "url": "/static/js/2.451eaecd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.451eaecd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57f66f140ddeef0aa6ed",
+    "url": "/static/js/main.1b03d46f.chunk.js"
+  },
+  {
+    "revision": "0114ec1e68421634cdeb",
+    "url": "/static/js/runtime-main.e773c126.js"
+  }
+]);
