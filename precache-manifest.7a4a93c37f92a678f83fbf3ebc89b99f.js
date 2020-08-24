f22ad2ec25db5444b1fe122d14832d06bce9582d
ShieldRich <richmeistergrad@yahoo.com>
2020-08-24 11:46:00 -0400

    Updates

---
+++ precache-manifest.7a4a93c37f92a678f83fbf3ebc89b99f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "537ebad962467a65230b301d68ea81b6",
+    "url": "/get-skill-quote-machine/index.html"
+  },
+  {
+    "revision": "b5a0cd8d8c793bf7d364",
+    "url": "/get-skill-quote-machine/static/css/main.87481de0.chunk.css"
+  },
+  {
+    "revision": "3157829e6f1b5879beab",
+    "url": "/get-skill-quote-machine/static/js/2.451eaecd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/get-skill-quote-machine/static/js/2.451eaecd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5a0cd8d8c793bf7d364",
+    "url": "/get-skill-quote-machine/static/js/main.578f82de.chunk.js"
+  },
+  {
+    "revision": "d3158c79a771cdb231b9",
+    "url": "/get-skill-quote-machine/static/js/runtime-main.1aad8b1f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f528bf440ff5d9a40df8326c9efa6ef3.js"
+  "/get-skill-quote-machine/precache-manifest.7a4a93c37f92a678f83fbf3ebc89b99f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/get-skill-quote-machine/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
